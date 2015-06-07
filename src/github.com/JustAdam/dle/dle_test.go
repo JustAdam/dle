c3af39c83c0ce5b611b040a91638b1bce89f6997
JustAdam <adambell7@gmail.com>
2015-06-07 13:03:51 +0200

    LogWriter Write test

--- src/github.com/JustAdam/dle/dle_test.go
+++ src/github.com/JustAdam/dle/dle_test.go
@@ -1,12 +1,31 @@
-// Copyright 2014 JustAdam (adambell7@gmail.com).  All rights reserved.
+// Copyright 2014, 2015 JustAdam (adambell7@gmail.com).  All rights reserved.
 // License: MIT
 
 package main
 
 import (
+	"bytes"
 	"testing"
 )
 
-func TestFail(t *testing.T) {
-	t.Fail()
+func TestLogWriterWrite(t *testing.T) {
+	token := "XX-XX"
+	lw := &LogWriter{
+		logline: make(chan []byte),
+		token:   token,
+	}
+
+	testWrites := []struct {
+		s, e []byte
+	}{
+		{[]byte("this is a test"), []byte(token + "this is a test")},
+	}
+
+	for _, v := range testWrites {
+		go lw.Write(v.s)
+		s := <-lw.logline
+		if !bytes.Equal(s, v.e) {
+			t.Errorf("Expecting %s, got %s", v.e, s)
+		}
+	}
 }
